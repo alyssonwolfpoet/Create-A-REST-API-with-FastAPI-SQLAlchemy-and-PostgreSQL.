9887a9ca0ee40e13543052690c8b66d34c76adae
Alysson <alyssos@hotmail.com>
2024-08-06 13:55:43 -0300

    feat: base tu api sqlalchemy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from database import SessionLocal
 
 app = FastAPI()
 
@@ -24,4 +25,12 @@ def get_an_item(item_id:int):
 
 @app.post("/items")
 def create_an_item():
+    pass
+
+@app.put("/item/{item_id}")
+def update_an_item(item_id:int):
+    pass
+
+@app.delete("item/{item_id}")
+def delete_item(item_id:int):
     pass
