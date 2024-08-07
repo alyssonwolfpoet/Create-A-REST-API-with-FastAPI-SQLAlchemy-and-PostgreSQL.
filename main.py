62d96d1a0333a7c982649f7878b8e187a357c2c3
Alysson <alyssos@hotmail.com>
2024-08-07 16:42:12 -0300

    feat: upgrade crud

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ def get_all_items():
     items = db.query(models.Item).all()
     return items
 
-@app.get("/items/{item_id}")
+@app.get("/item/{item_id}",response_model=Item,status_code=status.HTTP_200_OK)
 def get_an_item(item_id:int):
-    pass
+    item = db.query(models.Item).filter(models.Item.id==item_id).first()
+    return item
+
 
 @app.post("/items",response_model=Item,
           status_code=status.HTTP_201_CREATED)
@@ -49,10 +51,26 @@ def create_an_item(item:Item):
 
     return new_item
 
-@app.put("/item/{item_id}")
-def update_an_item(item_id:int):
-    pass
+@app.put("/item/{item_id}",response_model=Item,status_code=status.HTTP_200_OK)
+def update_an_item(item_id:int,item:Item):
+    item_to_update = db.query(models.Item).filter(models.Item.id==item_id).first()
+    item_to_update.name = item.name
+    item_to_update.description = item.description
+    item_to_update.price = item.price
+    item_to_update.on_offer = item.on_offer
+
+    db.commit()
+
+    return item_to_update
 
 @app.delete("item/{item_id}")
 def delete_item(item_id:int):
-    pass
+    item_to_delete=db.query(models.Item).filter(models.Item.id==item_id).first()
+
+    if item_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Resource Not Found")
+    
+    db.delete(item_to_delete)
+    db.commit()
+
+    return item_to_delete
